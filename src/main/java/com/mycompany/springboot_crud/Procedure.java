66d3141128885de6b5979830b22f0867a9670545
samarth2411 <73635998+samarth2411@users.noreply.github.com>
2022-09-15 23:00:47 +0530

    created procedure class for my own reference of the project connected database to the project and configured the database in application.properties file

---
+++ src/main/java/com/mycompany/springboot_crud/Procedure.java
@@ -0,0 +1,10 @@
+package com.mycompany.springboot_crud;
+
+public class Procedure {
+    // this is a class made for my own reference to tell how the spring boot project is created from start
+    // all the line of code here will be commented and is just for my own purpose
+    // STEP 1 -->
+    // Create a database schema and add database to the project(using mysql in this project) and then add schema created to the database
+    // Step 2 -->
+    // in application.proprties file configure the database
+}
