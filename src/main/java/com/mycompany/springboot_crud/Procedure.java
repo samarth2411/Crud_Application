7cfa3f4cfed16f75829bce6e54218d68c67142fb
samarth2411 <73635998+samarth2411@users.noreply.github.com>
2022-09-18 12:24:11 +0530

    create main controller class to handle the urls and created the handler showHomePage which opens up to index.html file created index.html file in the templates folder

--- src/main/java/com/mycompany/springboot_crud/Procedure.java
+++ src/main/java/com/mycompany/springboot_crud/Procedure.java
@@ -5,6 +5,23 @@ public class Procedure {
     // all the line of code here will be commented and is just for my own purpose
     // STEP 1 -->
     // Create a database schema and add database to the project(using mysql in this project) and then add schema created to the database
+
     // Step 2 -->
     // in application.proprties file configure the database
+
+    //Step 3 -->
+    // after configuring the application.properties file for the database just code the home page for application
+    // 1) Code the main controller class
+    // 2) code the index.html
+
+    //Step 4 -->
+    // cofigure intellij idea for spring boot dev tools
+    // press the shift key twice and type resistry and search for compiler.auto.make.allow.when.app.running enable and then set things in seetings build > compiler > build project automatically
+
+
+    //Step 5
+    // in order to use bootstrap we need to add some additional dependencies
+
+    //Step 6
+    // Code the data access layer (Repository Layer)
 }
